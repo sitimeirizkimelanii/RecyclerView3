60cb7c939621da2f6dd5d6614fb9c09410913164
sitimeirizkimelanii <siti_melani_23rpl@student.smktelkom-mlg.sch.id>
2016-11-13 23:10:39 +0700

    RecyclerView Delete

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.res.TypedArray;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -196,6 +197,21 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
     @Override
     public void doDelete(int pos) {
+        itemPos = pos;
+        final Hotel hotel = mList.get(pos);
+        mList.remove(itemPos);
+        // if (isFiltered) mListAll.remove(mListMapFilter.get(itemPos).intValue());
+        mAdapter.notifyDataSetChanged();
+        Snackbar.make(findViewById(R.id.fab), hotel.judul + " Terhapus", Snackbar.LENGTH_LONG)
+                .setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mList.add(itemPos, hotel);
+                        // if (isFiltered) mListAll.add(mListMapFilter.get(itemPos), hotel);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                })
+                .show();
 
     }
 
